9b0067f98e3b20bc0bcb81973e1bfc8947de860e
LuizGuilhes123 <luizguilhermeinformat@gmail.com>
2025-01-25 14:47:56 -0300

    feat: novos atributos na classe task #1

--- src/main/java/com/example/TodoListJava/entity/TaskEntity.java
+++ src/main/java/com/example/TodoListJava/entity/TaskEntity.java
@@ -1,4 +1,43 @@
 package com.example.TodoListJava.entity;
 
-public class TaskEntity {
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Getter
+@Setter
+@Entity
+@Table(
+        name = "tb_tarefa",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"usuario_id", "name"})
+)
+public class TaskEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = true)
+    private String name;
+
+    private BigDecimal cost;
+
+    private LocalDate dueDate;
+
+    private Integer position;
+
+    private Boolean favorite;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private UserEntity usuario;
+
 }
